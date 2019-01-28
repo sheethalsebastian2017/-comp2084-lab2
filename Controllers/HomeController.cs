5c65160a105659edab8140da5ebfff7705d2b024
sheethal sebastian <sheethalsebastian2017@gmail.com>
2019-01-28 16:48:36 -0500

    completing lab2

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using lab2.Models;
+
+namespace lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public dynamic Food { get; private set; }
+        public dynamic Tech { get; private set; }
+        public dynamic Sports { get; private set; }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    
+
+        public ActionResult CategoriesController()
+        {
+            {
+                var CategoriesController = new List<CategoriesController>();
+                CategoriesController.Add(Food);
+                CategoriesController.Add(Tech);
+                CategoriesController.Add(Sports);
+
+
+
+            }
+            
+                ViewBag.Food = Food;
+                ViewBag.Tech = Tech;
+                ViewBag.Sports = Sports;
+            
+    
+            return View();
+        }
+        public ActionResult ViewCategoriesController()
+        {
+            return View();
+        }
+
+    }
+}
+
+
+
---
+++ Models/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lab2.Models
+{
+    public class CategoriesController
+    {
+
+        public String Name { get; set; }
+
+        internal void Add(CategoriesController categoriesController)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Add(object categoriesControlle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
